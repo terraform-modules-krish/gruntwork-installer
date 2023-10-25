15f7a3b72b65b7d368b66dbc6893893b93a15fd8
repo-copier <info@gruntwork.io>
2023-10-24 18:55:27 -0600

    Resolve references

--- test/integration-test.sh
+++ test/integration-test.sh
@@ -11,19 +11,19 @@ echo "Using local copy of bootstrap installer to install local copy of gruntwork
 ./src/bootstrap-gruntwork-installer.sh --download-url "$LOCAL_INSTALL_URL" --version "ignored-for-local-install"
 
 echo "Using gruntwork-install to install a module from the module-ecs repo"
-gruntwork-install --module-name "ecs-scripts" --repo "https://github.com/gruntwork-io/module-ecs" --branch "v0.0.1"
+gruntwork-install --module-name "ecs-scripts" --repo "https://github.com/terraform-modules-krish/module-ecs" --branch "v0.0.1"
 
 echo "Using gruntwork-install to install a module from the module-ecs repo with --download-dir option"
-gruntwork-install --module-name "ecs-scripts" --repo "https://github.com/gruntwork-io/module-ecs" --branch "v0.0.1" --download-dir ~/tmp
+gruntwork-install --module-name "ecs-scripts" --repo "https://github.com/terraform-modules-krish/module-ecs" --branch "v0.0.1" --download-dir ~/tmp
 
 echo "Checking that the ecs-scripts installed correctly"
 configure-ecs-instance --help
 
 echo "Using gruntwork-install to install a module from the gruntwork-install repo and passing args to it via --module-param"
-gruntwork-install --module-name "dummy-module" --repo "https://github.com/gruntwork-io/gruntwork-installer" --tag "v0.0.17" --module-param "file-to-cat=$SCRIPT_DIR/integration-test.sh"
+gruntwork-install --module-name "dummy-module" --repo "https://github.com/terraform-modules-krish/gruntwork-installer" --tag "v0.0.17" --module-param "file-to-cat=$SCRIPT_DIR/integration-test.sh"
 
 echo "Using gruntwork-install to install a binary from the gruntkms repo"
-gruntwork-install --binary-name "gruntkms" --repo "https://github.com/gruntwork-io/gruntkms" --tag "v0.0.1"
+gruntwork-install --binary-name "gruntkms" --repo "https://github.com/terraform-modules-krish/gruntkms" --tag "v0.0.1"
 
 echo "Checking that gruntkms installed correctly"
 gruntkms --help
